d336a4d31c2e6e30243ffe59efd7fd7c63398d3c
ritika210502 <ritz21ch@gmail.com>
2025-07-29 16:13:38 +0530

    feat: datatypes like list,tuple,set and dictionary

---
+++ Datatypes/dictionary/basics.py
@@ -0,0 +1,55 @@
+# Creating a Dictionary
+# with Integer Keys
+Dict = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+print("\nDictionary with the use of Integer Keys: ")
+print(Dict)
+
+# Creating a Dictionary
+# with Mixed keys
+Dict = {'Name': 'Geeks', 1: [1, 2, 3, 4]}
+print("\nDictionary with the use of Mixed Keys: ")
+print(Dict)
+
+# Creating a Dictionary
+Dict = {'Dict1': {1: 'Geeks'},
+        'Dict2': {'Name': 'For'}}
+
+# Accessing element using key
+print(Dict['Dict1'])
+print(Dict['Dict1'][1])
+print(Dict['Dict2']['Name'])
+
+# demo for all dictionary methods
+dict1 = {1: "Python", 2: "Java", 3: "Ruby", 4: "Scala"}
+
+# copy() method
+dict2 = dict1.copy()
+print(dict2)
+
+# clear() method
+dict1.clear()
+print(dict1)
+
+# get() method
+print(dict2.get(1))
+
+# items() method
+print(dict2.items())
+
+# keys() method
+print(dict2.keys())
+
+# pop() method
+dict2.pop(4)
+print(dict2)
+
+# popitem() method
+dict2.popitem()
+print(dict2)
+
+# update() method
+dict2.update({3: "Scala"})
+print(dict2)
+
+# values() method
+print(dict2.values())
---
+++ Datatypes/set/programs.py
@@ -0,0 +1,41 @@
+#Check if Element Exists in a Set
+
+s = {10, 20, 30}
+print(20 in s)
+
+#Find Union of Two Sets
+a = {1, 2, 3}
+b = {3, 4, 5}
+print(a | b) 
+
+#Find Intersection of Two Sets
+a = {1, 2, 3}
+b = {2, 3, 4}
+print(a & b)  
+
+#Find Elements in One Set but Not in Another (Difference)
+
+a = {1, 2, 3}
+b = {2, 3, 4}
+print(a - b) 
+
+#Check if Two Sets are Disjoint
+a = {1, 2}
+b = {3, 4}
+print(a.isdisjoint(b))
+
+#Find Symmetric Difference Between Two Sets
+a = {1, 2, 3}
+b = {3, 4, 5}
+print(a ^ b)  # {1, 2, 4, 5}
+
+#Remove Duplicates from a List Using Set
+lst = [1, 2, 2, 3, 4, 4]
+unique = list(set(lst))
+print(unique) 
+
+#Check if One Set is Subset or Superset of Another
+a = {1, 2}
+b = {1, 2, 3, 4}
+print(a.issubset(b))   
+print(b.issuperset(a))
