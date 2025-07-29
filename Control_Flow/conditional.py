56a1af15e7fdc0f403f2396d589805a508339601
ritika210502 <ritz21ch@gmail.com>
2025-07-29 17:02:42 +0530

    feat: control flow and SRP

---
+++ Control_Flow/conditional.py
@@ -0,0 +1,16 @@
+# Program to check grade based on marks
+
+def check_grade(marks):
+    if marks >= 90:
+        return "Grade: A"
+    elif marks >= 75:
+        return "Grade: B"
+    elif marks >= 60:
+        return "Grade: C"
+    elif marks >= 40:
+        return "Grade: D"
+    else:
+        return "Grade: F (Fail)"
+
+student_marks = int(input("Enter student's marks: "))
+print(check_grade(student_marks))
