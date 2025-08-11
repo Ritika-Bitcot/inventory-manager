bb0a516f8bbb1f7553567d27379f884b5a729af1
Ritika-Bitcot <ritikachourasia@bitcot.com>
2025-08-11 13:30:37 +0530

    feat: get method to products

--- Week5/week3_api/routes/inventory.py
+++ Week5/week3_api/routes/inventory.py
@@ -1,12 +1,38 @@
+import os
+
 from flask import Blueprint, jsonify
+
+from Week3.core import Inventory
 
 inventory_bp = Blueprint("inventory", __name__)
 
+# Calculate absolute CSV path relative to this file for portability
 
-# note url will be /api/hello
+base_dir = os.path.dirname(os.path.abspath(__file__))
+csv_path = os.path.abspath(
+    os.path.join(base_dir, "..", "..", "..", "Week3", "data", "products.csv")
+)
+
+
+# Initialize the Inventory instance and load CSV once
+inventory = Inventory()
+inventory.load_from_csv(csv_path)
+
+
 @inventory_bp.route("/hello", methods=["GET"])
 def hello():
-    """
-    Simple hello endpoint to confirm blueprint setup.
-    """
     return jsonify({"message": "Hello from Inventory API!"}), 200
+
+
+@inventory_bp.route("/products", methods=["GET"])
+def get_products():
+    products_list = [product.dict() for product in inventory.products if product]
+    return jsonify(products_list), 200
+
+
+@inventory_bp.route("/products/<int:product_id>", methods=["GET"])
+def get_product(product_id):
+    product = next((p for p in inventory.products if p.product_id == product_id), None)
+    if not product:
+        return jsonify({"error": "Product not found"}), 404
+    return jsonify(product.dict()), 200
